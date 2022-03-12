84c35731188f9b0ad987e0549531ebb1c1726453
Egue <ing.fabian23@gmail.com>
2022-03-11 22:17:05 -0500

    final actualizacion del dia

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
   styleUrls: ['./progress.component.css' ]
 })
-export class ProgressComponent implements OnInit {
+export class ProgressComponent {
+  
+  progress:number = 50;
 
-  constructor() { }
+  get getPorcentaje()
+  {
+    return `${this.progress}%`;
+  }
 
-  ngOnInit(): void {
+  cambiarvalor(valor:number):number
+  {
+    if(this.progress >= 100 && valor >= 0)
+    {
+      return this.progress = 100;
+    }
+    if(this.progress <= 0 && valor < 0)
+    {
+      return this.progress = 0;
+    }
+    return this.progress = this.progress + valor;
   }
 
 }
